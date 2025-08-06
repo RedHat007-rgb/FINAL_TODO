969d395e507642bd5243f21189395c3813e84ed2
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-08-06 13:25:05 +0530

    Implemented trpc package

--- packages/trpc-config/src/server/server.ts
+++ packages/trpc-config/src/server/server.ts
@@ -1,36 +1,31 @@
 import { initTRPC } from "@trpc/server";
 import { z } from "zod";
-import { Status } from "@repo/shared";
 
 const t = initTRPC.create();
 const publicProcedure = t.procedure;
 
 const appRouter = t.router({
   todoRouter: t.router({
-    createTodo: publicProcedure
-      .input(
-        z.object({
-          title: z.string(),
-          description: z.string().optional(),
-          status: z.enum([
-            Status.COMPLETED,
-            Status.IN_PROGRESS,
-            Status.NOT_INTERESTED,
-          ] as const),
-        })
-      )
-      .output(
-        z.object({
-          title: z.string(),
-          description: z.string().optional(),
-          status: z.enum([
-            Status.COMPLETED,
-            Status.IN_PROGRESS,
-            Status.NOT_INTERESTED,
-          ] as const),
-        })
-      )
-      .mutation(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any),
-  }),
+    createTodo: publicProcedure.input(z.object({
+      id: z.string().optional(),
+      title: z.string(),
+      description: z.string().optional(),
+      status: z.enum([
+        Status.COMPLETED,
+        Status.IN_PROGRESS,
+        Status.NOT_INTERESTED,
+      ] as const),
+    })).output(z.object({
+      id: z.string().optional(),
+      title: z.string(),
+      description: z.string().optional(),
+      status: z.enum([
+        Status.COMPLETED,
+        Status.IN_PROGRESS,
+        Status.NOT_INTERESTED,
+      ] as const),
+    })).mutation(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any)
+  })
 });
 export type AppRouter = typeof appRouter;
+
